0dbb7a89cb10e53b0b48b1a41e33ff2dd1a11f42
manveer600 <100722147+manveer600@users.noreply.github.com>
2024-02-08 17:00:29 +0530

    adding axios instance correctly

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:9000';
+const BASE_URL = 'https://document-editing-system-backend2.onrender.com/';
 
 const axiosInstance = axios.create(); 
 axiosInstance.defaults.baseURL = BASE_URL;
